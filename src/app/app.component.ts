3592a797204ad56cbfa0ba18676b9bcb09a41ed5
Mario Querol <marioquerolamondarain@gmail.com>
2022-04-04 10:51:52 +0200

    Estructura básica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './features/models/product.model';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  product = new Product("Palmera", "https://prod-mercadona.imgix.net/images/5e949a6f64b52be230b8f1084156dcd9.jpg?fit=crop&h=300&w=300", 1, "a");
   title = 'componente-tienda-online-angular';
 }
